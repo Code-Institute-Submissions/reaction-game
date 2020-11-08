e4d1840a7b62740313978a532bd552e472995dfa
Helena-p <helenasleterkesto@gmail.com>
2020-11-08 18:06:47 +0000

    Adding animation to timer and colors at 10 and 5 seconds.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,5 +1,4 @@
  //Code credit to Udemy course Javascript Fundamentals by Laurence Svekis
- 
  //Declared Variables
  const container = document.querySelector(".container");
  const startGame = document.querySelector(".startGame");
@@ -8,6 +7,10 @@
  let player = {
      score : 0
  };
+ const FULL_DASH_ARRAY = 283;
+ const WARNING_THRESHOLD = 10;
+ const ALERT_THRESHOLD = 5;
+
 
  //Start button display and random delay before game start
 startGame.addEventListener("click", function() {
@@ -63,17 +66,41 @@ function makeItem() {
     container.appendChild(div);
 }
 
+//Credit: Mateusz Rybczonec, CSS-Trick for colored animation
+const COLOR_CODES = {
+    info: {
+        color: "green"
+    },
+    warning: {
+        color: "yellow",
+        threshold: WARNING_THRESHOLD
+    },
+    alert: {
+        color: "red",
+        threshold: ALERT_THRESHOLD
+    }
+};
+
 //Credit: Css-Tricks Animated Countdown Timer
 const TIME_LIMIT = 20;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
 
 document.getElementById("app").innerHTML = `
     <div class="base-timer">
         <svg class="base-timer__svg" viewbox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <g class="base-timer__circle">
                 <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"/>
+                <path id="base-timer-path-remaining" stroke-dasharray="283"
+                    class="base-timer__path-remaining ${remainingPathColor}"
+                    d="
+                    M 50, 50
+                    m -45, 0
+                    a 45,45 0 1,0 90,0
+                    a 45,45 0 1,0 -90,0 ">
+                </path>
             </g>
         </svg>
         <span id="base-timer-label" class="base-timer__label">
@@ -81,15 +108,25 @@ document.getElementById("app").innerHTML = `
         </span>
     </div>
 `;
+
 startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
 
 function startTimer() {
     timerInterval = setInterval(() => {
         timePassed = timePassed += 1;
         timeLeft = TIME_LIMIT - timePassed;
-
     document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
-    }, 1000);
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+}, 1000);
 }
 
 //Timer text display
@@ -102,3 +139,36 @@ function formatTime(time) {
     return `${minutes}:${seconds}`;
 }
 
+//Animation
+function setRemainingPathColor(timeLeft) {
+    const { alert, warning, info } = COLOR_CODES;
+    if (timeLeft <= alert.threshold) {
+        document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+    }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
